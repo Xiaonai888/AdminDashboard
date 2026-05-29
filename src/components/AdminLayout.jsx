5eab12af786408271e755d32162e54d12ed6c51b
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-30 05:51:26 +0700

    Update AdminLayout.jsx

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -257,6 +257,7 @@ const navGroups = [
       },
       { path: '/shadow-exclusive', label: 'Shadow Exclusive', icon: 'M12 2l7 4v6c0 5-3.5 9-7 10-3.5-1-7-5-7-10V6l7-4z M9 12l2 2 4-5' },
       { path: '/authors', label: 'Community', icon: 'M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2 M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8z' },
+      { path: '/stories', label: 'Stories', icon: 'M4 19.5A2.5 2.5 0 0 1 6.5 17H20 M4 4.5A2.5 2.5 0 0 1 6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5z' },
     ],
   },
   {
