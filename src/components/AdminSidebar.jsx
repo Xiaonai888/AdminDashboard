fb062f747cd46b073cf580c581b6a7d8f53617fc
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-20 16:18:33 +0700

    Update AdminSidebar.jsx

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import AdminSidebarSearch from './AdminSidebarSearch';.
+import AdminSidebarSearch from './AdminSidebarSearch';
 import { filterAdminNavSections } from './adminSidebarPermissions';
 
 const sidebarStyles = `
