18055e24f6ab5e97d0b3a6db4cf47a4123428726
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-22 16:25:37 +0700

    Update AdminIncomePage.jsx

--- src/pages/AdminIncomePage.jsx
+++ src/pages/AdminIncomePage.jsx
@@ -1146,7 +1146,7 @@ export default function AdminIncomePage() {
                       Author Payout Management
                     </div>
                     <div className="income-small">
-                      Episode Unlock + Diamond Gift
+                      Story Unlock Only · Minimum $10
                     </div>
                   </div>
 
